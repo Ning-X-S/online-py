152f0fe991a4d24ab0ee9dd2c564825ca0c6bbe3
higo <higo@higodeMacBook-Pro.local>
2020-04-05 16:32:39 +0800

    MOD:test

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+import os
 
-with cd('/home/work/creat-cli-app'):
-  run('git checkout .')
-  run('git pull')
-  run('npm install')
-  run('npm run build')
+os.system("cd /home/work/creat-cli-app")
+os.system("git pull")
+os.system("npm install")
+os.system("npm run build")
+
+# with cd('/home/work/creat-cli-app'):
+#   run('git checkout .')
+#   run('git pull')
+#   run('npm install')
+#   run('npm run build')
   # run('cp -rf dist/* dist_bak/')
   # run('pm2 restart react-app')
